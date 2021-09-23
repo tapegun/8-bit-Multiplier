65d955d7157d778dbf96f2344aa55aac35ff1a31
Chavez, Jorge <jorgejc2@illinois.edu>
2021-09-23 18:07:17 -0500

    processor and multiplier changes

--- Counter8.sv
+++ Counter8.sv
@@ -4,7 +4,11 @@ module counter8 (input reset, CLK, cntEn,
 		always @(posedge CLK)
 			if (reset)
 				Dout <= 3'b000;
+			else if (Dout[2] & Dout[1] & Dout[0])
+				Dout <= 3'b000;
 			else if (cntEn)
 				Dout <= Dout + 1;
+			else
+				Dout <= Dout;
 	
 endmodule 
