e0cb35072a7949cf00c983425c32689bddcb3be1
Chavez, Jorge <jorgejc2@illinois.edu>
2021-09-23 21:05:30 -0500

    changes to processor file

--- Processor.sv
+++ Processor.sv
@@ -19,63 +19,38 @@ module Processor (input logic   Clk,     // Internal
                                 BhexU);
 
 	 //local logic variables go here
-	 logic Reset_SH, LoadA_SH, LoadB_SH, Execute_SH;
-	 logic Ld_XA, Ld_B, newA, newB, opA, opB, bitA, bitB, Shift_En, add, sub
-	 logic [3:0] A, B, Din_S;
-	 logic x;
-	 //logic for counter
-	 logic cntEn;
-	 logic [2:0] curr_count;
+	 logic [3:0] A, B, Din_S; //dont know
 	 
-	 
-	 //We can use the "assign" statement to do simple combinational logic
-	 assign Aval = A;
-	 assign Bval = B;
-	 
-//	 assign LED = {Execute_SH,LoadA_SH,LoadB_SH,Reset_SH}; //Concatenate is a common operation in HDL
-	 assign x = 1'b0
-	 
-	 //Note that you can hardwire F and R here with 'assign'. What to assign them to? Check the demo points!
-	 //Remember that when you comment out the ports above, you will need to define F and R as variables
-	 //uncomment the following lines when you hardwaire F and R (This was the solution to the problem during Q/A)
-	 
-	 //Instantiation of modules here
-	 register_unit    reg_unit (
-                        .Clk(Clk),
-                        .Reset(Reset_SH),
-								.x,
-                        .Ld_XA, //note these are inferred assignments, because of the existence a logic variable of the same name
-                        .Ld_B,
-                        .Shift_En,
-                        .D(Switches),
-                        .A_In(newA),
-                        .B_In(newB),
-                        .A_out(opA),
-                        .B_out(opB),
-                        .A(A),
-                        .B(B) );
+	 logic LD_XA, LD_B, Shift_EN, Cnt_EN, Clr_XA, SUB_ADD; //outputs of control unit
+	 logic [2:0] curr_count; //output of counter
+	 logic x, M;
+	 logic newA, newB, opA, opB;
+
 					
 	//inputs: reset, CLK, cntEn
 	//outputs: [2:0] Dout
 	counter8 counter (.reset(), CLK(Clk), .cntEn(cntEn) //inputs
 							, .Dout(curr_count));					//outputs
 								
-	 control          control_unit (
-                        .Clk(Clk),
-                        .Reset(Reset_SH),
-                        .LoadA(LoadA_SH), 
-                        .LoadB(LoadB_SH),
-                        .Execute(Execute_SH),
-                        .Shift_En,
-								.add,
-								.sub,
-                        .Ld_XA,
-                        .Ld_B );
+	 control          control_unit (.Clk(Clk), .Reset(ClearALoadB), .Run(Execute), .M(M), .count(curr_count),
+						.LD_XA(LD_XA), .LD_B(LD_B), .Shift_EN(Shift_EN), .Cnt_EN(Cnt_EN), .Clr_XA(Clr_XA), .SUB_ADD(SUB_ADD));
 								
 								
 							//inputs: shift, add, sub, [7:0] A, [7:0] S, clearA_LoadB, clearA, clk, reset
 							//outputs: [7:0] Aout, x, m
-	 multiplier       values (.shift(Shift_En), .sub_add(sub_add),  .A(newA), .S(Switches), .clk(Clk), .Aout(A_val), .x(x))
+	 multiplier       values (.shift(Shift_En), .sub_add(SUB_ADD),  .A(newA), .S(Switches), .clk(Clk),
+										.Aout(A_val), .x(x));
+										
+	register_unit    reg_unit (
+                        .Clk(Clk),
+                        .Reset(Clr_XA),
+                                .x,
+                        .Ld_XA, //note these are inferred assignments, because of the existence a logic variable of the same name
+                        .Ld_B,
+                        .Shift_En,
+                        .D(Switches),
+                        .A(Aval),
+                        .B(Bval) );
 	 
 	 HexDriver        HexAL (
                         .In0(A[3:0]),
--- register_unit.sv
+++ register_unit.sv
@@ -2,10 +2,12 @@ module register_unit (input  logic Clk, Reset, x, Ld_XA, Ld_B,
                             Shift_En,
                       input  logic [7:0]  D,  
                       output logic [7:0]  A,
-                      output logic [7:0]  B);
+                      output logic [7:0]  B
+							 output logic M);
 
 	 logic x_;
-	 logic tmp_A_to_B
+	 logic tmp_A_to_B;
+	 assign M = B[0];
 	
     reg_8  reg_A (.*, 				.Shift_In(x_), 		  .Load(Ld_XA), .Shift_Out(tmp_A_to_B), .Data_Out(A));
     reg_8  reg_B (.*, .Reset(0), .Shift_In(tmp_A_to_B), .Load(Ld_B),  .Data_Out(B));
